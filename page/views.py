fe5dde691fcac1d9f6fc33a4984e966d51a78526
JG-C3 <jgc3django@gmail.com>
2022-10-14 09:28:07 +0900

    Update Django W6

--- page/views.py
+++ page/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-# [코드 작성] models.py의 Post 모델 불러오기
+# models.py의 Post 모델 불러오기
 from .models import Post
 
 # Create your views here.
@@ -7,10 +7,10 @@ def index(request):
     return render(request, 'page/index.html')
 
 def read(request):
-    # [코드 작성] Post 모델의 모든 객체를 리스트로 가져오기
+    # [미션] Post 모델의 모든 객체를 리스트로 가져오기
     posts = Post.objects.all()
     context = {
-        # [코드 작성] context에 posts 리스트를 딕셔너리 형식으로 넘겨주기
+        # [미션] context에 posts 리스트를 딕셔너리 형식으로 넘겨주기
         'posts': posts,
     }
     return render(request, 'page/read.html', context)
@@ -18,11 +18,11 @@ def read(request):
 def create(request):
     # 만약 form에서 값을 전송하는 방식이 'POST'일 경우
     if request.method == 'POST':
-        # [코드 작성] POST 방식으로 넘어온 값 중 'title'과 'content'의 값을 딕셔너리 형태로 반환
+        # [미션] POST 방식으로 넘어온 값 중 'title'과 'content'의 값을 딕셔너리 형태로 반환
         title = request.POST.get('title')
         content = request.POST.get('content')
 
-        # [코드 작성] Post 모델의 title과 content 필드값에 form으로 넘겨준 값을 저장하는 객체 생성
+        # [미션] Post 모델의 title과 content 필드값에 form으로 넘겨준 값을 저장하는 객체 생성
         Post.objects.create(
             title=title,
             content=content,
@@ -50,16 +50,17 @@ def update(request, id):
         title = request.POST.get('title')
         content = request.POST.get('content')
 
-        # [코드 작성] post 객체의 title과 content 필드값에 form으로 넘겨준 값을 넣어줌
+        # [코드 작성] post 객체의 title과 content 필드값에 form으로 넘겨준 값을 각각 넣어줌
         post.title = title
         post.content = content
 
         # [코드 작성] post 객체의 변경사항 저장
         post.save()
-        # 앱 이름이 'page'인 urls.py에서 name 속성값이 'read'인 url로 리다이렉트
+        # 앱 이름이 'page'인 urls.py에서 name 속성값이 'detail'인 url로 리다이렉트
         return redirect('page:detail', id)
     else:
         context = {
+            # [코드 작성] post 객체에 저장되어있는 값을 html에 넘겨주기
             'post': post,
         }
         return render(request, 'page/update.html', context)
