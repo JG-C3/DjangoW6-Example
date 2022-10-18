84f0fd3d8430ad52b98703c3bea368d441c1a521
JG-C3 <jgc3django@gmail.com>
2022-10-18 23:58:05 +0900

    Update Django W6

--- page/models.py
+++ page/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
 # Create your models here.
-# [코드 작성] Post 모델 작성
+# Post 모델 작성
 class Posting(models.Model):
     title = models.CharField(max_length=50)
     content = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
